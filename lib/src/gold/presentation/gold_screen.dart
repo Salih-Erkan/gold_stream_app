14bdd900345a301d4489255f05ffe2459cca2b3a
Salih-Erkan <s.erkan@mail.de>
2025-07-01 12:50:31 +0200

    Implement live gold price display using StreamBuilder

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
 
@@ -8,7 +9,7 @@ class GoldScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     /// Platzhalter für den Goldpreis
     /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    double goldPrice = 69.22;
+    double goldPrice;
 
     return SafeArea(
       child: Scaffold(
@@ -24,13 +25,31 @@ class GoldScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              StreamBuilder<double>(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(
+                      'Fehler beim Laden des Goldpreises',
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Colors.red,
+                      ),
+                    );
+                  } else if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  } else {
+                    goldPrice = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(
+                        locale: 'de_DE',
+                      ).format(goldPrice),
+                      style: Theme.of(context).textTheme.headlineLarge!
+                          .copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                    );
+                  }
+                },
               ),
             ],
           ),
